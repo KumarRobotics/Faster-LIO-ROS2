c93b42c744357a01343cab6f8461baf9653881fe
frankgon1627 <fg1333762@gmail.com>
2024-11-07 17:29:26 -0500

    adding conditional for rviz

--- faster-lio/launch/mapping_ouster128.launch.py
+++ faster-lio/launch/mapping_ouster128.launch.py
@@ -1,23 +1,27 @@
-import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.conditions import IfCondition
 from launch.actions import SetEnvironmentVariable, DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from os.path import expanduser
 import launch_ros.actions
 
 from launch.substitutions import EnvironmentVariable
-from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 def generate_launch_description():
-   
 
     stdout_linebuf_envvar = SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1')
     stdout_colorized_envvar = SetEnvironmentVariable('RCUTILS_COLORIZED_OUTPUT', '1')
     # use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-
+    
+    rviz = LaunchConfiguration('rviz')
+    rviz_use_arg = DeclareLaunchArgument(
+        'rviz',
+        default_value='False',
+        description='Whether to launch RVIZ'
+    )
+    
     config_file = PathJoinSubstitution([get_package_share_directory('faster_lio'), 'config', 'ouster128.yaml'])
     rviz_config = PathJoinSubstitution([get_package_share_directory('faster_lio'), 'rviz_cfg', 'loam_livox.rviz'])
     
@@ -28,18 +32,19 @@ def generate_launch_description():
         output='screen',
         parameters=[ config_file
                     ],
-   )
+    )
 
     rviz_node = Node(
-         package='rviz2',
-         executable='rviz2',
-         name='rviz2',
-         arguments=['-d', rviz_config,
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config,
                     '--ros-args', '--log-level', 'warn'],
-         parameters=[{'use_sim_time' : True}],
-         output='screen'
+        parameters=[{'use_sim_time' : True}],
+        output='screen',
+        condition=IfCondition(rviz_use_arg)
     )
-   
+
     ld = LaunchDescription([launch_ros.actions.SetParameter(name='use_sim_time', value=True),
                             ])
     # Set environment variables
